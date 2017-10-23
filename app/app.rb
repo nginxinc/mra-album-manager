dba71e9a1d6b4f7b2311c20004df667a13152128
cpretzer <charles@nginx.com>
2017-10-23 00:52:40 -0700

    * MRADEV-587: Add comments

--- app/app.rb
+++ app/app.rb
@@ -13,50 +13,83 @@ require './models/album.rb'
 require './models/image.rb'
 
 #
-#  app.rb
-#  AlbumManager
+# app.rb
+# AlbumManager
+# Unicorn based application which stores and retrieves information about a the
+# albums that an user has created
 #
-#  Copyright © 2017 NGINX Inc. All rights reserved.
+# Copyright © 2017 NGINX Inc. All rights reserved.
 #
 
+#
+# set the bind variable to this host
+#
 set :bind, '0.0.0.0'
 
+#
+# For development mode use the BetterErrors library
+#
 configure :development do
 	use BetterErrors::Middleware
 	BetterErrors.application_root = __dir__
 end
 
+#
+# Create helper methods
+#
 helpers do
+
+  #
+  # Read the request HTTP_AUTH_ID parameter in to a variable and return it
+  #
   def user_id
     @user_id ||= request.env['HTTP_AUTH_ID']
   end
 
+  #
+  # Retrieve the album specified by tne ID
+  #
   def album
-		@album ||= Album
-									 .includes(:images, :poster_image)
-									 .where(id: params[:id], user_id: user_id)
-									 .take || halt(404)
+    @album ||= Album.includes(:images, :poster_image)
+                   .where(id: params[:id], user_id: user_id)
+                   .take || halt(404)
 	end
 
+  #
+  # Retrieve the image specified by the id parameter
+  #
   def image
-		@image ||= Image
-									 .joins(:album)
-									 .where(id: params[:id], :albums => {:user_id => user_id})
-									 .take || halt(404)
+		@image ||= Image.joins(:album)
+                   .where(id: params[:id], :albums => {:user_id => user_id})
+                   .take || halt(404)
 	end
 end
 
+#
+# Before any request is processed, check that the path is "/" and that the
+# user_id is set from the helper method
+#
 before do
 	pass if request.path_info == '/'
 	halt 401, 'Auth-ID header is really required' if user_id.nil?
 	content_type 'application/json'
 end
 
+#
+# Handle get requests for the path "/".
+# This is used for health checks
+#
 get '/' do
 	'Sinatra is up!'
 end
 
-# list all
+#
+# Handle get requests for the path "/albums"
+# Find all pending albums in tne database and delete any older than
+# 15 minutes
+#
+# Then query for albums with a poster image and return their data as JSON
+#
 get '/albums' do
   time = Time.now
 
@@ -71,7 +104,11 @@ get '/albums' do
   albums.to_json(:include => :poster_image, :methods => :url)
 end
 
-# create
+#
+# Handle a post request to "/albums"
+# Create an album from the request body parameter named albums for the specified
+# user
+#
 post '/albums' do
 	album = Album.new(params['album'])
 
@@ -88,12 +125,18 @@ post '/albums' do
 	album.to_json(:include => [:images, :poster_image])
 end
 
-# view one
+#
+# Handle get requests for "/albums/XXX" where XXX is the unique ID of an album
+# in the database
+#
 get '/albums/:id' do
 	album.to_json(:include => [:images, :poster_image])
 end
 
-# update
+#
+# Handles a put request to "/albums/XXX" where XXX is the unique ID of an album
+# Updates the album with the data in the request payload
+#
 put '/albums/:id' do
 	album.update(params['album'])
 	album.save!
@@ -102,18 +145,28 @@ put '/albums/:id' do
   album.to_json(:include => [:images, :poster_image])
 end
 
+#
+# Handles a delete request for "/albums?xxx" where XXX is the unique ID of an album
+# Delete the album with the specified ID
+#
 delete '/albums/:id' do
 	Album.destroy(album.id)
 	status 202
 end
 
-# list all
+#
+# Handles a get request to "/images"
+# Retrieves all the images for a user and returns their data as JSON
+#
 get '/images' do
 	images = Image.joins(:album).where(:albums => {:user_id => user_id})
   images.to_json
 end
 
-# create
+#
+# Handles a post request for "/images"
+# Creates a new Image using the data in the request body and returns the data as JSON
+#
 post '/images' do
 	image = Image.new(params['image'])
 
@@ -129,12 +182,19 @@ post '/images' do
   image.to_json
 end
 
-# view one
+#
+# Handles a get request to "/images/XXX" where XXX is the ID of an image
+# Retrieves an image using the image helper and returns its data as JSON
+#
 get '/images/:id' do
 	image.to_json
 end
 
-# update
+#
+# Handles a PUT request for "/images/XXX" where XXX is the ID of an image
+# Uses the image helper to retrieve an image and updates it using the parameters
+# in the request payload
+#
 put '/images/:id' do
 	image.update(params['image'])
 
@@ -146,6 +206,10 @@ put '/images/:id' do
   image.to_json
 end
 
+#
+# Handles a DELETE request for "/images/XXX/YYY" where YYY is the UUID of an image
+# Removes an image from S3 and from the database
+#
 delete '/images/:id/:uuid' do
 	response = HTTParty.delete(request.env['UPLOADER_PHOTO'] + params[:uuid])
   response.to_json
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,16 +1,20 @@
 #
-#  album.rb
-#  AlbumManager
+# album.rb
+# AlbumManager
 #
-#  Copyright © 2017 NGINX Inc. All rights reserved.
+# The Album ActiveRecord model
 #
-
+# Copyright © 2017 NGINX Inc. All rights reserved.
+#
 class Album < ActiveRecord::Base
   has_many :images, :inverse_of => :album
   belongs_to :poster_image, :class_name => :Image, :foreign_key => 'poster_image_id'
 
   accepts_nested_attributes_for :images
 
+  #
+  # Generate the URL for an album by concatenating the ID: /albums/XXX
+  #
   def url
     "/albums/#{self.id}"
   end
