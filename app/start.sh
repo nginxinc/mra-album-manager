bddb565bb5f3de9d871ce112525d52d2c9da14b9
Eduard Borges <eduardborges@me.com>
2018-02-16 23:53:31 +0100

    MRADEV-693 router-mesh support

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-NGINX_PID=${NGINX_PID:-"/var/run/nginx.pid"}    # /   (root directory)
-NGINX_CONF=${NGINX_CONF:-"/etc/nginx/nginx.conf"}
-
 rake db:migrate
 
 APP=${APP:?"Please set the APP environment variable"}
@@ -9,24 +6,33 @@ APP=${APP:?"Please set the APP environment variable"}
 if [ "$NETWORK" = "fabric" ]
 then
     echo fabric configuration set;
+    NGINX_PID=${NGINX_PID:-"/var/run/nginx.pid"}    # /   (root directory)
+    NGINX_CONF=${NGINX_CONF:-"/etc/nginx/nginx.conf"}
+    nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;"
 fi
 
 echo "Running ${APP}"
 
 APP_PID=/var/run/unicorn.pid
 
-if [  -f "$APP_PID" ]; then
+if [ -f "$APP_PID" ]; then
     echo "Removing ${APP_PID}"
     rm -f $APP_PID
 fi
 
 $APP
 
-nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;"
-
 sleep 30
 
-while [ -f "$NGINX_PID" ] &&  [ -f "$APP_PID" ];
-do
-    sleep 5;
-done
+if [ "$NETWORK" = "fabric" ]
+then
+    while [ -f "$NGINX_PID" ] &&  [ -f "$APP_PID" ];
+    do
+        sleep 5;
+    done
+else
+    while [ -f "$APP_PID" ];
+    do
+        sleep 5;
+    done
+fi
